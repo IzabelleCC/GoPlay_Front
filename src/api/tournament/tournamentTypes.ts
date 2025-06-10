4174420ec6e55d5fd74914c5be858a3ec487d31c
Izabelle Cesario <izabellecesario@hotmail.com>
2025-06-10 01:33:41 -0300

    Adiciona autocomplete no endereço

--- src/api/tournament/tournamentTypes.ts
+++ src/api/tournament/tournamentTypes.ts
@@ -42,24 +42,6 @@ export interface Tournament {
     categories: TournamentCategory[];
 }
 
-// Seus payloads continuam os mesmos:
-
-export interface CreateTournamentPayload {
-    data: {
-        name: string;
-        description: string;
-        gamesStartDate: string; // ISO date
-        gamesEndDate: string;   // ISO date
-        registrationDeadline: string; // ISO date
-        paymentDeadline: string;      // ISO date
-        location: string;
-        registrationFee: number;
-        courtQuantity: number;
-        admUserId: string;
-        categories: TournamentCategory[];
-    };
-}
-
 export interface UpdateTournamentPayload {
     data: {
         id: number;
@@ -100,4 +82,25 @@ export interface InsertEliminationResultsPayload {
     competitorId2: number;
     game1: number;
     game2: number;
+}
+
+export interface CreateTournamentPayload {
+    data: {
+        name: string;
+        description: string;
+        gamesStartDate: string; // ISO date
+        gamesEndDate: string;   // ISO date
+        registrationDeadline: string; // ISO date
+        paymentDeadline: string;      // ISO date
+        location: string;
+        latitude: number;
+        longitude: number;
+        registrationFee: number;
+        courtQuantity: number;
+        admUserId: string;
+        categories: {
+            categoryType: string;
+            playerLimit: number;
+        }[];
+    };
 }
